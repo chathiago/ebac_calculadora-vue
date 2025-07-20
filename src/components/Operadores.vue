0b91fa64fdbcc7630623f8930f8e702f239271ae
Thiago Sam <thiagosamthi@gmail.com>
2025-07-20 12:20:32 -0400

    correção de classe duplicada

--- src/components/Operadores.vue
+++ src/components/Operadores.vue
@@ -7,9 +7,9 @@ export default {
 
 <template>
     <div class="d-block mb-4">
-        <button class="fw-bold btn fw-bold btn-outline-primary me-2" @click="$emit('update:modelValue', '+')">+</button>
-        <button class="fw-bold btn fw-bold btn-outline-primary me-2" @click="$emit('update:modelValue', '-')">-</button>
-        <button class="fw-bold btn fw-bold btn-outline-primary me-2" @click="$emit('update:modelValue', 'x')">x</button>
-        <button class="fw-bold btn fw-bold btn-outline-primary" @click="$emit('update:modelValue', '/')">/</button>
+        <button class="fw-bold btn btn-outline-primary me-2" @click="$emit('update:modelValue', '+')">+</button>
+        <button class="fw-bold btn btn-outline-primary me-2" @click="$emit('update:modelValue', '-')">-</button>
+        <button class="fw-bold btn btn-outline-primary me-2" @click="$emit('update:modelValue', 'x')">x</button>
+        <button class="fw-bold btn btn-outline-primary" @click="$emit('update:modelValue', '/')">/</button>
     </div>
 </template>
